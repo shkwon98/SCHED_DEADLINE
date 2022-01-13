8fd0a18893f73245335dc2e0e3f5ec7cda6613d7
shkwon98 <86837107+shkwon98@users.noreply.github.com>
2022-01-13 18:08:52 +0900

    Update sched_deadline.c

--- sched_deadline.c
+++ sched_deadline.c
@@ -1,21 +1,20 @@
-// Compiler flags: gcc name.c -o name -O0 -Wall -lm -lpthread
-
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-#include "sched_deadline.h"
+#include "ddd.h"
+
 
 void *rt_thread(void *data) {
- 	/* scheduler attributes */
+    	/* scheduler attributes */
 	struct sched_attr attr;
 	float period_ms = 1;
 
-	printf("Thread ID: [%ld]\n", gettid());
+	printf("deadline rt_thread started [%ld]\n", gettid());
 
 	/* set up sched_attr structure */
-	attr.size = sizeof(attr);
-	attr.sched_policy = SCHED_DEADLINE;
+    	attr.size = sizeof(attr);
+  	attr.sched_policy = SCHED_DEADLINE;
 	attr.sched_flags = 0;
 	attr.sched_nice = 0;
 	attr.sched_priority = 0;
@@ -23,33 +22,33 @@ void *rt_thread(void *data) {
 	attr.sched_period = attr.sched_deadline = period_ms * 1000 * 1000;
 
 	/* set sched deadline */
-  	if(sched_setattr(gettid(), &attr, 0)) {
+	if (sched_setattr(gettid(), &attr, 0)) {
 		perror("sched_setattr");
-	  	exit(1);
+		exit(1);
 	}
 
 	/* loop starts */
 	while (1) {
- 	/* WRITE YOUR CODE BELOW */
-       
-		sched_yield();
+        sched_yield();
+        //sched_wait_interval();
 	}
 
-    	return NULL;
+	printf("deadline rt_thread dies [%ld]\n", gettid());
+	return NULL;
 }
 
+int main (int argc, char **argv) {
+	pthread_t thread;
 
-int main(int argc, char* argv[]) {
+	printf("main thread started [%ld]\n", gettid());
+    
+    	/* Create a pthread with specified attributes */
+	pthread_create(&thread, NULL, rt_thread, NULL);
+	sleep(10);
+	
+    	/* Join the thread and wait until it is done */
+    	pthread_join(thread, NULL);
 
-  	pthread_t thread;
-
-  	/* Create a pthread with specified attributes */
-  	pthread_create(&thread, NULL, rt_thread, NULL);
-  	sleep(10);
-
-  	/* Join the thread and wait until it is done */
-  	pthread_join(thread, NULL);
-  
-  	printf("main dies");
-  	return 0;
+	printf("main thread dies [%ld]\n", gettid());
+	return 0;
 }
