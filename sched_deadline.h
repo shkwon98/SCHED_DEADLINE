ca96d26836b682551538a7f3ff0b34b8d04353b5
shkwon98 <86837107+shkwon98@users.noreply.github.com>
2022-01-13 18:08:01 +0900

    Update sched_deadline.h

--- sched_deadline.h
+++ sched_deadline.h
@@ -5,38 +5,37 @@
 #define _GNU_SOURCE
 #endif
 
-#include <stdint.h>      /* int*_t */
-#include <sys/syscall.h> /* SYS_* */
-#include <unistd.h>      /* syscall() */
+#include <unistd.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
 
 struct sched_attr {
-	uint32_t size;
-	uint32_t sched_policy;
-	uint64_t sched_flags;
+    	__u32 size;
+	__u32 sched_policy;
+	__u64 sched_flags;
 
 	/* used by: SCHED_NORMAL, SCHED_BATCH */
-	int32_t sched_nice;
+	__s32 sched_nice;
 
 	/* used by: SCHED_FIFO, SCHED_RR */
-	uint32_t sched_priority;
+	__u32 sched_priority;
 
 	/* used by: SCHED_DEADLINE (nsec) */
-	uint64_t sched_runtime;
-	uint64_t sched_deadline;
-	uint64_t sched_period;
+	__u64 sched_runtime;
+	__u64 sched_deadline;
+	__u64 sched_period;
 };
 
 /* sched_policy */
 #define SCHED_DEADLINE 6
 
 /* helpers */
-#define gettid()      syscall(SYS_gettid)
-#define sched_yield() syscall(SYS_sched_yield)
-static inline int sched_setattr(pid_t pid, const struct sched_attr *attr, unsigned int flags) {
-	return syscall(SYS_sched_setattr, pid, attr, flags);
+#define gettid() syscall(__NR_gettid)
+int sched_setattr(pid_t pid, const struct sched_attr *attr, unsigned int flags) {
+    return syscall(__NR_sched_setattr, pid, attr, flags);
 }
-static inline int sched_getattr(pid_t pid, struct sched_attr *attr, unsigned int size, unsigned int flags) {
-	return syscall(SYS_sched_getattr, pid, attr, size, flags);
+int sched_getattr(pid_t pid, struct sched_attr *attr, unsigned int size, unsigned int flags) {
+    return syscall(__NR_sched_getattr, pid, attr, size, flags);
 }
 
 #endif /* SCHED_DEADLINE_H */
